fedc02d2b0215c3d70ee02f54571fe6689c9536c
sharmatamann <sharmatamann@ISCN5CG12163TL>
2022-03-21 18:15:40 +0530

    changes till video 85

---
+++ src/components/MovieList.vue
@@ -0,0 +1,40 @@
+<template>
+        <div id="movie-list">
+            <movie-item v-for="movie in filteredMovies" v-bind:movie="movie.movie" ></movie-item>
+         </div>
+</template>
+<script>
+   import genres from '../util/genres';
+   import MovieItem from './MovieItem.vue';
+    export default {
+      props: [ 'genre', 'time' , 'movies' ],
+      methods:{
+          moviePassesGenreFilter(movie){
+              if(!this.genre.length)
+              {
+                  return true;
+              }
+              else{
+                let movieGenres=movie.movie.Genre.split(",");
+                let matched= true;
+                this.genre.forEach(genre => {
+                  if(movieGenres.indexOf(genre) === -1)
+                  {
+                    matched=false;
+                  }
+                  
+                });
+                return matched;
+              }
+            }
+      },
+      computed:{
+          filteredMovies(){
+              return this.movies.filter(this.moviePassesGenreFilter);
+          }
+      },
+      components:{
+        MovieItem
+      }
+    }
+</script>
